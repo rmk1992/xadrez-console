ccc2e6d97af1d876b5553bbfa5cb8f95bd63f415
enderson <rmk.enderson@gmail.com>
2022-08-30 22:08:10 -0300

    Implementing board

---
+++ Tabuleiro/Piece.cs
@@ -0,0 +1,18 @@
+﻿namespace Tabuleiro
+{
+    internal class Piece
+    {
+        public Position Position { get; set; }
+        public Color Color { get; protected set; }
+        public int MovementsQuantity { get; protected set; }
+        public Board Board { get; protected set; }
+
+        public Piece(Position position, Color color, Board board)
+        {
+            Position = position;
+            Color = color;
+            Board = board;
+            MovementsQuantity = 0;
+        }
+    }
+}
