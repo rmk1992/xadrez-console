76f5aeb223380f7c57d1cecf59c00e46bfc139dd
enderson <rmk.enderson@gmail.com>
2022-08-31 20:48:57 -0300

    Better implementation of PrintBoard method

--- Program.cs
+++ Program.cs
@@ -4,13 +4,13 @@ using Xadrez.ChessLayer;
 
 try
 {
-    Board Board = new Board(8, 8);
+    Board board = new Board(8, 8);
 
-    Board.PutPiece(new King(Board, Color.White), new Position(2, 4));
-    Board.PutPiece(new Tower(Board, Color.White), new Position(5, 2));
-    Board.PutPiece(new Tower(Board, Color.White), new Position(6, 4));
+    board.PutPiece(new King(board, Color.White), new Position(2, 4));
+    board.PutPiece(new Tower(board, Color.Blue), new Position(5, 2));
+    board.PutPiece(new Tower(board, Color.White), new Position(6, 4));
 
-    Screen.PrintBoard(Board);
+    Screen.PrintBoard(board);
 }
 catch (BoardException e)
 {
--- Screen.cs
+++ Screen.cs
@@ -8,6 +8,7 @@ namespace Xadrez
         {
             for (int i = 0; i < board.Rows; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < board.Columns; j++)
                 {
                     if (board.Piece(i, j) == null)
@@ -16,10 +17,27 @@ namespace Xadrez
                     }
                     else
                     {
-                        Console.Write(board.Piece(i, j) + " ");
+                        PrintPiece(board.Piece(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static void PrintPiece(Piece piece)
+        {
+            if (piece.Color == Color.White)
+            {
+                Console.Write(piece);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write(piece);
+                Console.ForegroundColor = aux;
             }
         }
     }
